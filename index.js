abd685263df730aeae0125f962182126f7b01f7e
zain <muhammadzain8@gmail.com>
2021-06-21 21:37:51 +0500

    basic video code added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express=require('express')
 const app=express()
 const server=require("http").createServer(app);
+
 const cors=require('cors');
+
 const { Socket } = require('dgram');
 
+//  socket.io   used for real-time data transmession data can be ( messages , audio , video)
 const io=require('socket.io')(server,{
     cors:{
         origin:"*",
@@ -13,27 +16,36 @@ const io=require('socket.io')(server,{
 
 app.use(cors())
 
-const PORT=process.env.PORT || 5000
+
+const PORT=process.env.PORT || 5002
 
 app.get('/',(req,res)=>{
     res.send(" server running ")
 })
 
 io.on('connection',(socket)=>{
+    
+    // emit myslef
     socket.emit('me',socket.id)
 
+    // endCall 
     socket.on('disconnect',()=>{
         socket.broadcast.emit("callended")
     })
 
-    socket.on("calluser",({usertoCall,signalData,from,name})=>{
-        io.to(usertoCall).emit("calluser",{signal:signalData,from,name})
+    //  get-date from frontend and giveto this 
+    //  Id-of-user,user-data,from,name
+    socket.on("calluser",({userToCall,signalData,from,name})=>{
+        // sending data to frontend
+        io.to(userToCall).emit("calluser",{signal:signalData,from,name})
+
     })
 
+    //  in this we recieve the data and pass the signal 
     socket.on("answercall",(data)=>{
         io.to(data.to).emit("callaccepted",data.signal)
     })
-    
+      
 })
 
 server.listen(PORT,()=>{
